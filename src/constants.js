84926b9a414137973667621fbed22abdbaeef777
Sultan <sultan.toktomambetov.04@bk.ru>
2020-09-07 22:10:53 +0600

    installed redux , react-redux packages , created reducer.

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const PIN_CODE = "11223344";
+
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  answer: "pending",
+  value: "",
+};
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
